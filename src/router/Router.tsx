16d2f15f801cf1cd75bd5d2d0f61c8f95a6b5949
Jihoon-Yoon96 <03470@naver.com>
2025-09-30 16:00:24 +0900

    개발환경 세팅 완료 2

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,18 +1,17 @@
-// router/router.tsx
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import routes from "./routes";
 
-const Router: React.FC = () => {
+const Router = (): React.JSX.Element => {
     return (
         <BrowserRouter>
             <Routes>
                 {routes.map((route) => (
-                    <Route path={route.path} element={route.element} />
+                    <Route key={route.path} path={route.path} element={route.element} />
                 ))}
             </Routes>
         </BrowserRouter>
     );
-}
+};
 
 export default Router;
